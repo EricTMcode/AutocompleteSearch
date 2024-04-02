635ecfb317e3c9264d1992d02e6c0110eeff56df
Eric <ebagnaro@gmail.com>
2024-04-02 15:03:35 +0200

    Initial Commit

---
+++ AutocompleteSearch/AutocompleteSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutocompleteSearchApp.swift
+//  AutocompleteSearch
+//
+//  Created by Eric on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct AutocompleteSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
